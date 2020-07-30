9d8e23c88d50567179f6b8a312399db46d833334
Jessica_Jones <jessicamayblack@gmail.com>
2020-07-30 17:24:08 -0400

    work

--- assets/app.js
+++ assets/app.js
@@ -7,4 +7,16 @@ $(document).ready(function(){
   $('a[data-menu-close]').click(function(event) {
     $('.menu').removeClass('open');
   });
+
+  // work
+
+  $('a[data-work]').on('mouseenter', function(event) {
+    var $this = $(this);
+    var imageURL = $this.data('work-img-url');
+    var caption = $this.data('work-img-caption');
+
+    $('[data-work-img]').css("background-image", "url(" + imageURL + ")");
+    $('[data-work-caption]').text(caption);
+  });
 });
+
